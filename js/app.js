baa98ea391650629917f1ff8eacc017cde122eda
Friday Godswill <faradayyg@gmail.com>
2018-05-25 19:47:57 +0100

    Added hint to the range slider thumb

--- js/app.js
+++ js/app.js
@@ -1,28 +1,25 @@
-var app = angular.module("sliderApp",[]);
+const app = angular.module("sliderApp",[]);
 
-app.controller("sliderController", function($scope,$http) {
+app.controller("sliderController", ($scope,$http) => {
 
 	//Variable Declarations and initializations
 	$scope.revenue 		= 0;
 	$scope.percentage 	= 100;
 	$scope.topCustomers = 12;
-	var startDate 		= '2017-01-01';
-	var secretKey  		= getSecretKey();
+	const startDate 	= '2017-01-01';
+	const secretKey  	= getSecretKey();
 
 	//Get date of today 
-	var dateObject 		= new Date();
-	var endDate 		= `${dateObject.getFullYear()}-${dateObject.getMonth()}-${dateObject.getDate()}`;
+	let dateObject 		= new Date();
+	const endDate 		= `${dateObject.getFullYear()}-${dateObject.getMonth()}-${dateObject.getDate()}`;
 
 	//Pluralization fix for customer(s)
-	$scope.isPlural = function(count) {
-		if(count > 1) {
-			return 's';
-		}
-		return;
+	$scope.isPlural = (count) => {
+		return count > 1 ? 's': '';
 	}
 
 	//Function to calculate percentage revenue
-	$scope.calculatePercentage = function() {
+	$scope.calculatePercentage = () => {
 		if($scope.sorted_transactions.length !=0) {
 			$currentRange = $scope.getCurrentRange('total_transaction_amount', $scope.sorted_transactions, $scope.topCustomers);
 			$percentage = $currentRange/$scope.revenue * 100;
@@ -38,12 +35,29 @@ app.controller("sliderController", function($scope,$http) {
 		* @param Position the position to count down from
 		* @return a Summed up something
 	**/
-	$scope.getCurrentRange = function(key,object,position) {
-		var total_amount = 0;
-		for (var i = position - 1; i >= 0; i--) {
+	$scope.getCurrentRange = (key,object,position) => {
+		let total_amount = 0;
+		for (let i = position - 1; i >= 0; i--) {
 			total_amount += object[i][key];
 		}
 		return total_amount;
+	}
+
+	$scope.setHint 		= () => {
+		let slider 		= document.querySelector('#rangeSlider');
+		let newPoint 	= (slider.value - slider.getAttribute("min")) / (slider.getAttribute("max") - slider.getAttribute("min"));
+		let newPlace 	= slider.offsetWidth * newPoint;
+		let offset 		= 1.2;
+		let hintDiv 	= document.querySelector('#hint');
+
+		offset -= newPoint;
+		hintDiv.style.left = newPlace+"px";
+		if($scope.topCustomers == 1 ||$scope.topCustomers == 12) {
+			hintDiv.style.display = "none";
+		}
+		else {
+			hintDiv.style.display = "inline-block";
+		}
 	}
 
 	//Get and Set the total Transaction Amount
@@ -57,16 +71,16 @@ app.controller("sliderController", function($scope,$http) {
 		 headers: {
 		 	'Authorization': `Bearer ${secretKey}`
 		 }
-		}).then(r=>{
+		}).then(response=>{
 
 			//Calculate total amount by all customers
 			var total_transaction_amount = 0;
-			r.data.data.forEach((customer)=> {
+			response.data.data.forEach((customer)=> {
 				total_transaction_amount += customer.total_transaction_amount;
 			});
 
 			//Sort transactions according to total transaction amount descending order
-			$scope.sorted_transactions = r.data.data.sort((a,b)=>{
+			$scope.sorted_transactions = response.data.data.sort((a,b)=>{
 				return b.total_transaction_amount - a.total_transaction_amount;
 			});
 
